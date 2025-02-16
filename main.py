838203b2bc4a0282dd8f828ab1d950227acac95b
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-16 15:34:00 +0200

    added functionality to validate currency symbol, added type annotations

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
-import locale
 from sys import exit
+from unicodedata import category
+
 
 def main() -> None:
-    locale.setlocale(locale.LC_ALL, '')
-    currency_symbol = locale.localeconv()['currency_symbol']
+    exit_message: str = 'Exiting program...'
     while True:
         try:
-            user_input = input('Enter a cost: ')
+            user_input: str = input('Enter a cost: ')
 
             if user_input == 'exit':
-                print('Thanks for trying my program!')
+                print(exit_message)
                 exit()
 
-            cost = float(user_input[1:])
+            cost: float = float(user_input[1:])
+
+            currency_symbols: list[str] = [
+                ch for ch in user_input if category(ch) == 'Sc']
+
+            if user_input[0] in currency_symbols and len(currency_symbols) == 1:
+                currency_symbol: str = currency_symbols[0]
+            else:
+                print('Please enter a valid cost...')
+                continue
 
             if cost <= 0:
-                print('Invalid cost...')
+                print('Please enter a valid cost...')
                 continue
 
             user_input = input('Enter a country or state tax rate: ')
 
             if user_input[-1] == '%':
-                tax_rate = float(user_input[:-1])/100
+                tax_rate: float = float(user_input[:-1])/100
             else:
-                tax_rate = float(user_input)
+                tax_rate: float = float(user_input)
 
             if tax_rate < 0:
                 print('Invalid tax rate...')
                 continue
 
-            tax = round(tax_rate * cost, 2)
+            tax: float = round(tax_rate * cost, 2)
 
-            total_cost = round(cost + tax, 2)
+            total_cost: float = round(cost + tax, 2)
 
             print(
                 f'The tax is {currency_symbol}{tax:.2f}, and the total cost with tax is {currency_symbol}{total_cost:.2f}.')
+
         except ValueError:
-            print('Invalid input...')
+            print('Please enter valid input...')
             continue
+
         except KeyboardInterrupt:
-            print('Exiting...')
+            print(exit_message)
             exit()
 
 
